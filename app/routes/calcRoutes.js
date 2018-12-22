f08cc7db95db1417708e248f0fd37b866b71ad50
Abhishek Kaushik <abhishek.kaushik@news.co.uk>
2018-12-22 19:21:24 +0530

    minor change

--- app/routes/calcRoutes.js
+++ app/routes/calcRoutes.js
@@ -1,8 +1,25 @@
-const { sortTree } = require('../sortTree');
-const {solve} = require('../solve');
+const cors = require('cors');
 
-module.exports = function(app) {
+const {
+    sortTree
+} = require('../sortTree');
+const {
+    solve
+} = require('../solve');
+
+module.exports = function (app) {
+
+    app.use(function(req, res, next) {
+            res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+            next();
+        });
+
     app.post('/calc', (req, res) => {
-        res.send({"result" : solve(sortTree(req.body.expression)).toString()});
+        let obj = Object.keys(req.body)[0];
+        const exp = JSON.parse(obj).expression
+        res.send({
+            "result": solve(sortTree(exp))
+        });
     });
 };
